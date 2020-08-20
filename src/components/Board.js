5cbe33d6276bfbdbb934115d3923f37c61946db1
drewatienza <atienza.drew@gmail.com>
2020-08-19 17:00:35 -0700

    added components folder with Board.js file

---
+++ src/components/Board.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Board = () => {
+    return (
+        <div>
+
+           <div>
+               <button></button>
+               <button></button>
+               <button></button>
+           </div>
+           <div>
+               <button></button>
+               <button></button>
+               <button></button>
+           </div>
+           <div>
+               <button></button>
+               <button></button>
+               <button></button>
+           </div>
+
+        </div>
+    )
+}
+
+export default Board
+
+<div></d
+<div></d
+<div></d
